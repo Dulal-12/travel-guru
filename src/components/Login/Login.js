697b8a8bc1c214de625e4bce2047a874257304da
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-09-18 21:09:22 +0600

    done google and fb pop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 const Login = () => {
+    //usestate for google and facebook
+    const [detail,setDetail] = useState({
+        isSignIn : false,
+    })
+    const [fbDetail,setfbDetail] = useState({
+        isSignIn : false,
+    })
+
+//firebase Provider
+    const provider = new firebase.auth.GoogleAuthProvider();
+    var fbProvider = new firebase.auth.FacebookAuthProvider();
+
+    const hadleGoogleSignIn = ()=>{
+        firebase.auth().signInWithPopup(provider)
+        .then(result=> {
+           const newDetail = {...detail};
+           newDetail.isSignIn = true;
+           setDetail(newDetail);
+        })}
+
+    const hadleGoogleSignOut = ()=>{
+        const newDetail = {...detail};
+        newDetail.isSignIn = false;
+        setDetail(newDetail);}
+
+
+
+   const handleFbSignIn =()=>{
+     firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+        const newDetail = {...fbDetail};
+        newDetail.isSignIn = true;
+        setfbDetail(newDetail);
+        console.log(result.user)
+        })
+   }
+
+    const handleFbSignOut = ()=>{
+       const newDetail = {...fbDetail};
+       newDetail.isSignIn = false;
+       setfbDetail(newDetail);
+   }
+
+     
 
     const handleFieldInput = (e)=>{
        
-        if(e.target.name === "email"){
+        if(e.target.name === "email")
+        {
             const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
             
         }
-        if(e.target.name === "password"){
+        if(e.target.name === "password")
+        {
             const isPasswordValid = (e.target.value).length>6 && ( /[a-z]\d|\d[a-z]/i).test( e.target.value);
         }
         
     }
    
     return (
-        <div>
+        <div >
              <form className="description">
                  <h1 style={{ fontFamily: 'Satisfy'}}>Login Form</h1>
                         <p>Email :</p>
@@ -27,7 +77,18 @@ const Login = () => {
                         <br/>
                         <br/>
                         <input type="submit" className="btn btn-primary" value="Submit"/>
+                        
               </form>
+             
+              <div>
+                  {
+                  detail.isSignIn?<button className="btn1"  onClick={hadleGoogleSignOut}>Sign Out </button>:
+                  <button className="btn1"   onClick={hadleGoogleSignIn}>Sign In </button>
+                  }<br/><br/>
+                  {fbDetail.isSignIn?<button className="btn1"   onClick={handleFbSignOut}>Sign Out</button>:
+                  <button className="btn1"  onClick={handleFbSignIn}>Sign In</button>}
+              </div>
+            
         </div>
     );
 };
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAc7NdlT5sIQq71vG4Ii7bQjmkDbmZpv0g",
+    authDomain: "travel-guru-78827.firebaseapp.com",
+    databaseURL: "https://travel-guru-78827.firebaseio.com",
+    projectId: "travel-guru-78827",
+    storageBucket: "travel-guru-78827.appspot.com",
+    messagingSenderId: "230296504514",
+    appId: "1:230296504514:web:a0385d1f01c4e371d6135b"
+  };
+  export default firebaseConfig;
